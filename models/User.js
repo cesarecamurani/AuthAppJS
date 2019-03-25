8f5ed2e693ec9f22650b578f0cb72e1c9c71e567
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-25 11:10:02 +0000

    Created route for posting login requests - setup passport

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  username: { type: String, required: true },
   date: { type: Date, default: Date.now }
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const app = express();
+const passport = require("passport");
+const users = require("./routes/api/users");
 const port = process.env.PORT || 5000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const db = require("./config/keys").mongoURI;
+const app = express();
+
+require("./config/passport")(passport);
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
-app.listen(port, () => 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(bodyParser.json());
+app.use("/api/users", users);
+
+app.listen(port, () =>
   console.log(`Server up and running on port ${port} !`));

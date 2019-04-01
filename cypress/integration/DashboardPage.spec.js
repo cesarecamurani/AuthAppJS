07f8977e61ed6c6c9573514f90398727bb5c4415
Cesare Camurani <cesare.camurani@gmail.com>
2019-04-01 11:46:57 +0100

    Dashboard tests for displaying right elements passing

--- cypress/integration/DashboardPage.spec.js
+++ cypress/integration/DashboardPage.spec.js
@@ -1,1 +1,18 @@
 // Tests for Dashboard page
+
+describe ('Tests for Dashboard Page', () => {
+  describe ('Elements on the page', () => {
+    it ('checks for elements to be present', () => {
+      cy.visit('/login')
+      cy.get('#email').type('cesare@makers.com')
+      cy.get('#password').type('qwerty')
+      cy.get('.btn').click()
+      cy.contains('MERN Auth App')
+      cy.contains('Hey there, Luca')
+      cy.contains('You are logged into a full-stack MERN app')
+    });
+    it ('checks for logout button to link to login page', () => {
+
+    });
+  });
+});

a8d81e4671b0febff74feb82362004737961ed92
Cesare Camurani <cesare.camurani@gmail.com>
2019-04-01 18:45:24 +0100

    App name changed in Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
               className="col s5 brand-logo center black-text"
             >
               <i className="material-icons">how_to_reg</i>
-              MERN Auth App
+              AuthAppJS
             </Link>
           </div>
         </nav>

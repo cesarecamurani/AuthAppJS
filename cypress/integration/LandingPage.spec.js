f6d8f8261a1100e5280b147269243eab21d5d62d
Cesare Camurani <cesare.camurani@gmail.com>
2019-04-01 10:47:01 +0100

    Test for register/login button passing

---
+++ cypress/integration/LandingPage.spec.js
@@ -0,0 +1,25 @@
+describe('Test Landing Page', () => {
+  describe ('Visit the landing page', () => {
+    it ('landing page loads correctly', () => {
+      cy.visit('/api/users');
+    });
+  });
+  describe ('Elements on the page', () => {
+    it ('checks for "MERN Auth App" to be present', () => {
+      cy.contains('MERN Auth App')
+    });
+    it ('checks for "Welcome to my first auth app made with the MERN" Stack to be present', () => {
+      cy.contains('MERN Auth App').click()
+      cy.contains('Welcome to my first auth app made with the MERN Stack')
+    });
+  });
+  describe('Clickable buttons', () => {
+    it('checks for register button to link to register page', () => {
+      cy.contains('Register').click()
+      
+    })
+    it('checks for login button to link to login page', () => {
+      cy.contains('Log In')
+    })
+  })
+})

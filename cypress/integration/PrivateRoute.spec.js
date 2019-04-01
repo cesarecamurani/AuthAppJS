cc0e2633ad8524591a89a9ef58f95f976a4d3f33
Cesare Camurani <cesare.camurani@gmail.com>
2019-04-01 11:54:48 +0100

    Private route updated

--- cypress/integration/PrivateRoute.spec.js
+++ cypress/integration/PrivateRoute.spec.js
@@ -3,7 +3,7 @@
 describe('Test Private Route', () => {
   describe('cannot access the dashboard if not logged in', () => {
     it('redirect you to the login page', () => {
-      cy.url('http//localhost:3000/dashboard')
+      cy.visit('http//localhost:3000/dashboard')
       cy.url('http//localhost:3000/login')
     })
   })

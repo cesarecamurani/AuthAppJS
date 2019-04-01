1466ca0652ddbff242c3a8f576911a3331324f0b
Cesare Camurani <cesare.camurani@gmail.com>
2019-04-01 11:48:42 +0100

    Test for logout redirecting to login page passing

--- cypress/integration/DashboardPage.spec.js
+++ cypress/integration/DashboardPage.spec.js
@@ -12,7 +12,8 @@ describe ('Tests for Dashboard Page', () => {
       cy.contains('You are logged into a full-stack MERN app')
     });
     it ('checks for logout button to link to login page', () => {
-
+      cy.contains('Logout').click()
+      cy.url('/login')
     });
   });
 });

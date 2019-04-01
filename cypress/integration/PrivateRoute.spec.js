12fdf80b4eef4f844da80d57a9f10f72b2c28e20
Cesare Camurani <cesare.camurani@gmail.com>
2019-04-01 11:09:49 +0100

    Test for private route redirecting to login passing

---
+++ cypress/integration/PrivateRoute.spec.js
@@ -0,0 +1,9 @@
+
+describe('Private Route', () => {
+  describe('trying to access the dashboard if not logged in should redirect you to the login page', () => {
+    it('redirect you to the login page', () => {
+      cy.url('http//localhost:3000/dashboard')
+      cy.url('http//localhost:3000/login')
+    })
+  })
+})
